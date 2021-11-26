dd74ebaaf50ee1a104ebd6f932d44b504718c1a2
Khrystyna Bohdan <khrystyna.bohdan@softblues.io>
2021-11-26 13:48:19 +0200

    added filter

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import multiselect from "./modules/multiselect";
+import allquestions from "./modules/allquestions";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: {
-    multiselect,
-  },
+  modules: { allquestions },
   state: {
     questions: [
       {
