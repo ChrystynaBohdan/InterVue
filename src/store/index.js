14ac78f6b5ba516afa962596b8c2f0669cf63f6d
Khrystyna Bohdan <khrystyna.bohdan@softblues.io>
2021-12-01 12:37:19 +0200

    added moments

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   modules: { allquestions },
   state: {
-    isLogged: true,
+    isLogged: false,
     currentUser: { name: "User" },
     questions: [
       {
