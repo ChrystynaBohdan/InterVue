a22385b1da9d46f9c2f6206c22909b6830bda567
Khrystyna Bohdan <khrystyna.bohdan@softblues.io>
2021-12-06 12:16:23 +0200

    add author to form

--- src/views/QuestionForm.vue
+++ src/views/QuestionForm.vue
@@ -1,31 +1,37 @@
 <template>
   <div class="flex flex-col mt-32 max-w-xl min-h-screen py-24">
-    <form
-      v-on:submit.prevent
-      class="flex flex-col gap-4 text-sm"
-    >
+    <form v-on:submit.prevent class="flex flex-col gap-4 text-sm">
       <h2 class="font-bold text-3xl">Add New Question Here</h2>
-      <label class="flex flex-col gap-2"> Title:
+      <h1 class="text-xs font-extralight text-gray-400">by Volodymyr Sen</h1>
+      <label class="flex flex-col gap-2">
+        Title:
         <input
           type="text"
           placeholder="Question's title"
           class="rounded-md py-3 px-2 outline-none border border-solid border-gray-300"
-        >
+        />
       </label>
 
       <div class="h-20 w-3/5">
-        <label class="flex flex-col gap-2">Grade:
-          <Multiselect :options="gradeOptions" v-model="selectedGrade" :placeholder="'Select a technology'"/>
+        <label class="flex flex-col gap-2"
+          >Grade:
+          <Multiselect :options="gradeOptions" v-model="selectedGrade" :placeholder="'Select a technology'" />
         </label>
       </div>
 
       <div class="h-20 w-3/5">
-        <label class="flex flex-col gap-2"> Technology:
-          <Multiselect :options="categories" v-model="selectedCategory" :placeholder="'Select a grade for a question'" />
+        <label class="flex flex-col gap-2">
+          Technology:
+          <Multiselect
+            :options="categories"
+            v-model="selectedCategory"
+            :placeholder="'Select a grade for a question'"
+          />
         </label>
       </div>
 
-      <label class="flex flex-col gap-2"> Question's body:
+      <label class="flex flex-col gap-2">
+        Question's body:
         <textarea
           required
           rows="3"
@@ -34,7 +40,8 @@
         ></textarea>
       </label>
 
-      <label class="flex flex-col gap-2">Code snippet
+      <label class="flex flex-col gap-2"
+        >Code snippet
         <textarea
           rows="4"
           class="rounded-md py-3 px-2 outline-none border border-solid border-gray-300"
@@ -44,42 +51,38 @@
 
       <div class="flex justify-end gap-4">
         <button
-            type="button"
-            class="
+          type="button"
+          class="
             p-2
             cursor-pointer
             bg-white
-            hover:bg-gray-500
-            hover:text-white
+            hover:bg-gray-500 hover:text-white
             rounded-md
             text-black
-            border
-            border-grey-100
+            border border-grey-100
             transition
             duration-500
             ease-in-out
-            "
-          >
+          "
+        >
           Reset
         </button>
 
         <button
-            type="submit"
-            class="
+          type="submit"
+          class="
             p-2
             cursor-pointer
             bg-white
-            hover:bg-gray-500
-            hover:text-white
+            hover:bg-gray-500 hover:text-white
             rounded-md
             text-black
-            border
-            border-grey-100
+            border border-grey-100
             transition
             duration-500
             ease-in-out
-            "
-          >
+          "
+        >
           Submit
         </button>
       </div>
@@ -88,7 +91,7 @@
 </template>
 
 <script>
-import Multiselect from '../components/Multiselect.vue'
+import Multiselect from "../components/Multiselect.vue";
 
 export default {
   name: "QuestionForm",
@@ -113,8 +116,8 @@ export default {
       ],
 
       selectedCategory: [],
-      selectedGrage: []
-    }
-  }
+      selectedGrage: [],
+    };
+  },
 };
 </script>
