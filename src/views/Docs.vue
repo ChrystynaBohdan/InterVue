79196caf02b4be643ccab5ec9b85d14efae663ed
Khrystyna Bohdan <khrystyna.bohdan@softblues.io>
2021-12-07 18:01:18 +0200

    fixed docs styles

--- src/views/Docs.vue
+++ src/views/Docs.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="router-size">
-    <router-link :to="{ name: 'Grade2' }" class="ml-6">Grades</router-link>
-    <router-link :to="{ name: 'Technologies' }" class="ml-6">Technologies</router-link>
+  <div class="flex flex-col gap-y-6">
+    <div class="flex gap-x-5 text-base text-black font-bold cursor-pointer">
+      <router-link :to="{ name: 'Grade2' }" class="border-b border-transparent hover:border-current hover:text-gray-500"
+        >Grades</router-link
+      >
+      <router-link
+        :to="{ name: 'Technologies' }"
+        class="border-b border-transparent hover:border-current hover:text-gray-500"
+        >Technologies</router-link
+      >
+    </div>
     <router-view></router-view>
   </div>
 </template>
--- src/views/Grades2.vue
+++ src/views/Grades2.vue
@@ -1,12 +1,37 @@
 <template>
-  <div class="router-size">
-    <router-link :to="{ params: { technology: 'Javascript' } }" class="ml-6">Javascript</router-link>
-    <router-link :to="{ params: { technology: 'React' } }" class="ml-6">React</router-link>
-    <router-link :to="{ params: { technology: 'Vue' } }" class="ml-6">Vue</router-link>
-    <router-link :to="{ params: { technology: 'Angular' } }" class="ml-6">Angular</router-link>
-    <router-link :to="{ params: { technology: 'PHP' } }" class="ml-6">PHP</router-link>
-    <router-link :to="{ params: { technology: 'Wordpress' } }" class="ml-6">Wordpress</router-link>
-
+  <div class="flex flex-col gap-y-4">
+    <div class="flex gap-x-10 text-gray-400">
+      <router-link
+        :to="{ params: { technology: 'javascript' } }"
+        class="border-b border-transparent hover:border-current hover:text-black font-bold"
+        >Javascript</router-link
+      >
+      <router-link
+        :to="{ params: { technology: 'react' } }"
+        class="border-b border-transparent hover:border-current hover:text-black font-bold"
+        >React</router-link
+      >
+      <router-link
+        :to="{ params: { technology: 'vue' } }"
+        class="border-b border-transparent hover:border-current hover:text-black font-bold"
+        >Vue</router-link
+      >
+      <router-link
+        :to="{ params: { technology: 'angular' } }"
+        class="border-b border-transparent hover:border-current hover:text-black font-bold"
+        >Angular</router-link
+      >
+      <router-link
+        :to="{ params: { technology: 'php' } }"
+        class="border-b border-transparent hover:border-current hover:text-black font-bold"
+        >PHP</router-link
+      >
+      <router-link
+        :to="{ params: { technology: 'wordpress' } }"
+        class="border-b border-transparent hover:border-current hover:text-black font-bold"
+        >Wordpress</router-link
+      >
+    </div>
     <Categories :technology="selectedTechnology" :levels="categories[selectedTechnology]"></Categories>
   </div>
 </template>
@@ -24,47 +49,60 @@ export default {
   data() {
     return {
       categories: {
-        Javascript: {
-          junior: "text one",
-          middle: "lorem ipsum",
-          senior: "senior level",
-          lead: "asdas",
-          architect: "asdasd",
+        javascript: {
+          junior: "Here we learn JavaScript, starting from scratch and go on to advanced concepts like OOP.",
+          middle: "Here we learn JavaScript, starting from scratch and go on to advanced concepts like OOP.",
+          senior: "Here we learn JavaScript, starting from scratch and go on to advanced concepts like OOP.",
+          lead: "Here we learn JavaScript, starting from scratch and go on to advanced concepts like OOP.",
+          architect: "Here we learn JavaScript, starting from scratch and go on to advanced concepts like OOP.",
         },
-        React: {
-          junior: "zxczxc",
-          middle: "zxczxc",
-          senior: "vcxvwq",
-          lead: "2134",
-          architect: "asda",
+        react: {
+          junior:
+            "React Native combines the best parts of native development with React, a best-in-class JavaScript library",
+          middle:
+            "React Native combines the best parts of native development with React, a best-in-class JavaScript library ",
+          senior:
+            "React Native combines the best parts of native development with React, a best-in-class JavaScript library ",
+          lead: "React Native combines the best parts of native development with React, a best-in-class JavaScript library ",
+          architect:
+            "React Native combines the best parts of native development with React, a best-in-class JavaScript library ",
         },
-        Vue: {
-          junior: "",
-          middle: "",
-          senior: "",
-          lead: "",
-          architect: "",
+        vue: {
+          junior: "Vue.js is an MIT-licensed open source project with its ongoing development made possible entirely",
+          middle: "Vue.js is an MIT-licensed open source project with its ongoing development made possible entirely",
+          senior: "Vue.js is an MIT-licensed open source project with its ongoing development made possible entirely",
+          lead: "Vue.js is an MIT-licensed open source project with its ongoing development made possible entirely",
+          architect:
+            "Vue.js is an MIT-licensed open source project with its ongoing development made possible entirely",
         },
-        Angular: {
-          junior: "",
-          middle: "",
-          senior: "",
-          lead: "",
-          architect: "",
+        angular: {
+          junior:
+            "Angular puts you in control over scalability. Meet huge data requirements by building data models on RxJS",
+          middle:
+            "Angular puts you in control over scalability. Meet huge data requirements by building data models on RxJS",
+          senior:
+            "Angular puts you in control over scalability. Meet huge data requirements by building data models on RxJS",
+          lead: "Angular puts you in control over scalability. Meet huge data requirements by building data models on RxJS",
+          architect:
+            "Angular puts you in control over scalability. Meet huge data requirements by building data models on RxJS",
         },
-        PHP: {
-          junior: "",
-          middle: "",
-          senior: "",
-          lead: "",
-          architect: "",
+        php: {
+          junior:
+            "PHP is a server side scripting language. that is used to develop Static websites or Dynamic websites or Web applications.",
+          middle:
+            "PHP is a server side scripting language. that is used to develop Static websites or Dynamic websites or Web applications.",
+          senior:
+            "PHP is a server side scripting language. that is used to develop Static websites or Dynamic websites or Web applications.",
+          lead: "PHP is a server side scripting language. that is used to develop Static websites or Dynamic websites or Web applications.",
+          architect:
+            "PHP is a server side scripting language. that is used to develop Static websites or Dynamic websites or Web applications.",
         },
-        Wordpress: {
-          junior: "",
-          middle: "",
-          senior: "",
-          lead: "",
-          architect: "",
+        wordpress: {
+          junior: "All created from professional WordPress developers and marketers to perfectly fit your business.",
+          middle: "All created from professional WordPress developers and marketers to perfectly fit your business.",
+          senior: "All created from professional WordPress developers and marketers to perfectly fit your business.",
+          lead: "All created from professional WordPress developers and marketers to perfectly fit your business.",
+          architect: "All created from professional WordPress developers and marketers to perfectly fit your business.",
         },
       },
     };
